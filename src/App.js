354d97a216125c441e0c2639db5e31a7f29027b2
sgoraczynski <s.goraczynski@interia.pl>
2021-10-29 12:05:34 +0200

    add header with logo and links to navigate

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import HomePage from '../src/pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import { Switch, Route } from 'react-router-dom';
 import AboutPage from './pages/aboutpage/aboutpage.component.jsx';
+import Header from './components/header/header-component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
         <Route path='/about' component={AboutPage} />
-        <Route path='/*' component={ShopPage} />
+        <Route path='/*' component={HomePage} />
       </Switch>
     </div>
   );
