ed6c16f742bdaaa38198d0ef95a3b674a92d6996
sgoraczynski <s.goraczynski@interia.pl>
2021-11-04 14:45:01 +0100

    integrate firebase, create sign in page with sign in and sign in with google buttons plus styles

--- src/App.js
+++ src/App.js
@@ -6,20 +6,44 @@ import { Switch, Route } from 'react-router-dom';
 import AboutPage from './pages/aboutpage/aboutpage.component';
 import Header from './components/header/header-component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-        <Route path='/about' component={AboutPage} />
-        <Route path='/*' component={HomePage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null
+  
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route path='/about' component={AboutPage} />
+          <Route path='/*' component={HomePage} />
+        </Switch>
+      </div>
+    )
+  }
+  
 }
 
 export default App;
